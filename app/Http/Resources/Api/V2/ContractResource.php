28f00184b168c10eb134445b0eb965eee5603d5b
ebrahim abdallah <hemareda122@gmail.com>
2026-07-08 14:56:24 +0300

    your commit message

--- app/Http/Resources/Api/V2/ContractResource.php
+++ app/Http/Resources/Api/V2/ContractResource.php
@@ -45,9 +45,11 @@ class ContractResource extends JsonResource
             'is_draft' => (bool) $this->is_draft,
             'step' => $this->step,
             'contract_status_id' => $this->contract_status_id,
-            'contract_status_name' => optional($this->contractStatus)->name
+            'contract_status_name' => optional($this->contractStatus)->name ??'قيد المراجعة'
                 ? trans(optional($this->contractStatus)->name)
                 : null,
+                'contract_status_color' => optional($this->contractStatus)->color ??'#000000',
+                'contract_status_icon' => optional($this->contractStatus)->icon ??'<i class="fa fa-check"></i>',
             'number_of_units_in_realestate' => $this->numberOfUnitsInRealestate(),
             'created_at' => optional($this->created_at)->format('Y-m-d'),
         ]);
