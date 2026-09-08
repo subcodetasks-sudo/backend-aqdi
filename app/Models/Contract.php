b96ccfd3d3f1347da283adf8a002f6ea71b9ba23
ebrahim abdallah <hemareda122@gmail.com>
2026-09-08 14:51:05 +0300

    last update

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -13,7 +13,6 @@ class Contract extends Model
     use AdminSearchableContract;
     use HasFactory;
 
-    $table = 'contracts-new';
     public const SKIP_INITIAL_STEPS_INSTRUMENT_TYPES = [
         'lease_renewal',
         'sublease_agreement',
@@ -101,8 +100,6 @@ class Contract extends Model
     | GLOBAL VARIABLES
     |--------------------------------------------------------------------------
     */
-
-    protected $table = 'contracts_new';
 
     protected $guarded = ['id'];
 
